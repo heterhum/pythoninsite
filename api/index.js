847c656ff52c164852d1c9a337ee141be104f5b3
heterhum <160785637+heterhum@users.noreply.github.com>
2024-12-17 20:56:57 +0100

    BUG CSS

--- api/index.js
+++ api/index.js
@@ -2,33 +2,25 @@
 /* eslint-env ,ode, es6*/ 
 
 
-// Importe le paquet express
+
 const express = require('express')
-
 const { spawn } = require('node:child_process');
-
-// Créé une application express
 const app = express()
-
-// Démarrer le serveur et écouter un port donné
 const PORT = 6300
 
 // Importer la logique de la page d'accueil
 const genererPageAccueil = require('./pages/index-get.js')
 
 function runPythonScript(scriptPath, args, callback) {
-    const pythonProcess = spawn('python', [scriptPath].concat(args));
- 
+    const pythonProcess = spawn('python', [scriptPath].concat(args)); //comme dans un terminale
     let data = '';
     pythonProcess.stdout.on('data', (chunk) => {
         data += chunk.toString(); // Collect data from Python script
     });
- 
-    pythonProcess.stderr.on('data', (error) => {
+    pythonProcess.stderr.on('data', (error) => { //gere les erreurs
         console.error(`stderr: ${error}`);
     });
- 
-    pythonProcess.on('close', (code) => {
+    pythonProcess.on('close', (code) => { //renvoie la donnée (magie noir)
         if (code !== 0) {
             console.log(`Python script exited with code ${code}`);
             callback(`Error: Script exited with code ${code}`, null);
@@ -39,27 +31,20 @@ function runPythonScript(scriptPath, args, callback) {
     });
 }
 
-// Ecoute la méthode GET et la route '/'
+// Genere page html de l'acceuil + css + js +img ect ...
 app.get('/', async(req, res) => {
-    // Récupérer le contenu de la page HTML
     const indexHtml = await genererPageAccueil()
-    
-
-  // Envoyer le résultat
   res.send(indexHtml)
 })
+app.use('/styles',express.static('../public/styles'))
+app.use('/img',express.static('../public/img'))
 
-// Retourne les images
-// Retourne les styles
-app.use('/styles',express.static('C:/Users/xoxar/Desktop/perso/code/site/pythonsite/public/styles'))
-app.use('/img',express.static('C:/Users/xoxar/Desktop/perso/code/site/pythonsite/public/img'))
-
+//permet de recevoir et enovyé des json depuis /submit
 app.use(express.json())
 app.post('/submit', async (req, res) => {
-    const data = await req.body; // Les données envoyées par le client via fetch()
-    console.log('Données reçues:', parseInt(data["value"])); // Affiche les données dans le serveur
-
-    runPythonScript('C:/Users/xoxar/Desktop/perso/code/site/pythonsite/server/calcul.py', [parseInt(data["value"])], (err, result) => {
+    const data = await req.body; // Les données envoyées par le js public via fetch()
+    console.log('Données reçues:', parseInt(data["value"]));
+    runPythonScript('../server/calcul.py', [parseInt(data["value"])], (err, result) => {
         if (err) {
             console.log(err); }
         else {
@@ -68,10 +53,11 @@ app.post('/submit', async (req, res) => {
                 receivedData: result
             });
         }
-    })
+    }) //on vient de vérifié si y'a une erreur et de renvoyé la donné traité
   });
 
+
+//démmarage !
 app.listen(PORT, () => {
-  // Fonction éxecutée lorsque l'application a démarré
   console.log(`Serveur démarré : http://localhost:${PORT}`)
 })
--- public/styles/script.js
+++ public/styles/script.js
@@ -1,18 +1,19 @@
 'use strict'
 /* eslint-env ,ode, es6*/ 
 
+//création d'une fonction qui va etre appellé dans le html
 function numbers() {
-    fetch('http://localhost:6300/submit', {
+    fetch('http://localhost:6300/submit', { //endroit ou enovyé
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json' // Indique que le corps de la requête est du JSON
+          'Content-Type': 'application/json' 
         },
-        body: JSON.stringify({value: 4 }) // Corps de la requête sous forme JSON
+        body: JSON.stringify({value: 4 }) // Magie noir JSON aka dictionnaire
       })
         .then((response) => response.json()) // On attend une réponse au format JSON
         .then(data => {
-          console.log('Réponse du serveur:', parseInt(data.receivedData));
-          document.write(parseInt(data.receivedData))
+          console.log('Réponse du serveur:', parseInt(data.receivedData)); //console du naviguateur
+          document.write(parseInt(data.receivedData)) //askip sa change la page, a changé
         })
 }
 
